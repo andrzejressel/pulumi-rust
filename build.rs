44cd1cc816d2864b26204afd1503f86e2e3e2535
Andrzej Ressel <github@andrzejressel.pl>
2026-03-16 23:11:10 +0100

    CI

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(
-        &["../pulumi-ast/package.proto", "../pulumi-ast/pcl.proto"],
-        &["../pulumi-ast"],
+        &["pulumi-ast/package.proto", "pulumi-ast/pcl.proto"],
+        &["pulumi-ast"],
     )?;
     Ok(())
 }
